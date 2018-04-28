a0ef21f86ba872e425a8cc733bd097aee89c1cd0
David Preece <davep@zedkep.com>
2018-04-28 18:29:58 +1200

    Renaming and better comments

--- model.py
+++ model.py
@@ -16,14 +16,14 @@ import math
 
 
 class VoxelData:
-    def __init__(self, size, *, scale=None):
+    def __init__(self, size, scale):
         """Create voxel data.
 
         :param size: A (x, y, z) tuple of raw data size.
         :param scale: A scalar describing the size of each voxel cube."""
         self.data = np.empty(size, dtype=np.float32)
         self.size = size
-        self.scale = 1 if scale is None else scale
+        self.scale = scale
 
     def real_world_coords(self, x, y, z):
         """Return real world coordinates for an (x, y, z) tuple.
@@ -48,7 +48,15 @@ class VoxelData:
     def save(self, filename):
         """Save the data in raw voxel format.
 
-        :param filename: Name of the file, will append the extension"""
+        :param filename: Name of the file, will append the extension.
+
+        Your best bet with this is to open it in Paraview (https://www.paraview.org/) and...
+        * confirm that it is raw
+        * set it as float data that's little endian
+        * there's only one channel (pressure)
+        * and the extents are 0-63 in all three directions.
+
+        Add a contour and create isosurfaces at (say) 0.02 (i.e. 2% additional pressure over atmospheric)."""
         with open(filename + '.raw', 'wb') as f:
             self.data.tofile(f)
 
@@ -58,13 +66,12 @@ class SoundPressureField:
     max_pressure = 0.2
     min_pressure = -0.2
 
-    def __init__(self, location, frequency, amplitude, *, time=0):
+    def __init__(self, location, frequency, amplitude):
         """A function representing a sound pressure field created by a sinusoidal wave.
 
         :param location: an (x, y, z) tuple representing the centre of the field in metres.
         :param frequency: the frequency.
-        :param amplitude: a scalar pressure relative to atmosphere.
-        :param time: a time offset in seconds."""
+        :param amplitude: a scalar pressure relative to atmosphere."""
         self.location = np.array(location, dtype=np.float32)
         self.duration = 1.0 / frequency
         self.wavelength = self.duration * SoundPressureField.v_sound
@@ -77,13 +84,14 @@ class SoundPressureField:
         diff = coords - self.location
         distance_squared = diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2]
         distance = math.sqrt(distance_squared)
+        distance_due_to_delay = self.offset() * SoundPressureField.v_sound
 
         # bail out now if we're going to have a division by zero
         if distance_squared == 0:
             return SoundPressureField.max_pressure
 
         # calculate phase difference due to distance (in radians)
-        phase = (distance / self.wavelength) * 2 * math.pi
+        phase = ((distance + distance_due_to_delay) / self.wavelength) * 2 * math.pi
         rtn = (math.sin(phase) * (self.amplitude / distance_squared))
         return max(SoundPressureField.min_pressure, min(rtn, SoundPressureField.max_pressure))
 
@@ -94,7 +102,7 @@ voxels = VoxelData((64, 64, 64), scale=0.001)
 pressure_fields = list()
 for x in range(0, 4):
     for y in range(0, 4):
-        pressure_fields.append(SoundPressureField((0.008 + x*0.016, 0.008 + y*0.016, 0), 40000, 0.00001))
+        pressure_fields.append(SoundPressureField((0.008 + x*0.016, 0.008 + y*0.016, 0), 40000, 0.00001, x * 0.00001))
 
 functions = list()
 for field in pressure_fields:
